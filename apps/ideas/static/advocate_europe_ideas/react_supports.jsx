f6636448222d9a2e74d24d0e8883a6a02a3038eb
fuzzylogic2000 <katharina.albers@liqd.de>
2020-01-08 15:31:08 +0100

    initial: rename files and folders from advocate_europe to civic_europe

--- apps/ideas/static/advocate_europe_ideas/react_supports.jsx
+++
@@ -1,8 +0,0 @@
-let SupportButton = require('./SupportButton')
-let React = require('react')
-let ReactDOM = require('react-dom')
-
-module.exports.renderSupports = function (el) {
-  let props = JSON.parse(el.getAttribute('data-attributes'))
-  ReactDOM.render(<SupportButton {...props} />, el)
-}
