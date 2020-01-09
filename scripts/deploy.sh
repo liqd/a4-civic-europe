7af14fa4cd3dfbb77e3f5df0e00ae2d2c9224b26
fuzzylogic2000 <katharina.albers@liqd.de>
2020-01-09 16:10:27 +0100

    travis: enable deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,16 +5,16 @@ set -e -v
 if [ -n ${TRAVIS_SSH_SECRET} ]; then
     SSH_ID_ARG="-i ${HOME}/id_rsa"
     cat <<EOF | openssl enc -d -aes-256-cbc -pbkdf2 -pass env:TRAVIS_SSH_SECRET -a -out ~/id_rsa
-U2FsdGVkX18u2lLhir31yxnC61lZhVioi12A6W9gjJhjiyETLVQ84/haZhspgDzt
-HFk9V+9JiJGWakrpbH5GexMQDvgzUm4tntw2xU/2LoDBb0iP3HOOYu44Y0VGsTU6
-Wf+vBBE5fOuTpAE0XRla9gEeK9pImhxDDybpgLoPI2x8EqsCaizM/7BtuwfsF5It
-juZB/z3wokkhe0AsQpOk3FPOmzMcW3ILhYHquvzeU4G79muGYNobhan+pv6uvtER
-n7NrFa/oKopa4nJC9Q3fP+itJmTVBaEFuH5vJQWZtSsNvMOPCNq6eXevBInc8aca
-a+6wmHVAp3YI5eWPqmO34c3eo5JSfwJDiJ9tRP6q9w/uR3lkvPq4FVef2ccDfWHe
-jEzDMGdjZJIX0X3WZVQYSnjY3dB4vd8ptHJkdDP2Oj8JTF39CRkYWmNWB68Xa6sf
-7vZ1Q3o3njRGe6R5LrMOQHLR4eMv9f8SjXl8Hifmbk/vqXd9aZD3n75f6ggQR+if
-ywfoxlB+aACB3xQM40xY6LHXeE8YbO1hbpAqF23T72yoLTrPjQ0xi9SvP8oNmsNh
-u9yspubt1Jha/GR03caafw==
+U2FsdGVkX1+/BL7aPLwnE7RyGgR1slqsocmNwPw+LXsUauHpU5bSbmJn//dQfEEL
+uc51C6dKEA0/rv+8aPnehn8qx44iOOvnW9vfdur6tdtgkvioAsXonjTy1llzSlm4
+6rQMXjTv08FtkGlvB3rTuNArTD/V5vy5mzywOc8F+UGvW2d9NDB7140ss9oLl9HQ
+I5zqU24w02ZK565w3KBnThSRcWxhRnIxnN/mFlr+VOGNK7zRHlTHTVAJVCsXMUeP
+1vVG/PMMFLHhRoElpnXkLXyQAA9izVR9QKSoShXRJodVMJO+tBpvuKx4KkX1ypBB
+JjaMQ3DH6V8x96bcAx8T6QBeHDpxrKfbM4fO1mm/dCHa4Iu+0h9HL4mzMQBlFoZ4
+11UXHZUY37qzbM6cFTxVemcEKdMJsAzDiExa3ZKtY8SDtN+s5xYVmxdW6CvZx+ri
+L67FJhq6QDPLzyy5E/0xdD1PYerHy99i7BGVMXPYHJHOU2BuY6kivvfNddqinVBu
+yqDl1uVWdbqm8WdEb4wjbtxcsZuYclrsxC23+Rar3W8xYXHTpPrMJ4yZlbS1m7+W
+LK2tA2wn8/WMMvqFzcMMXw==
 EOF
     chmod 600 ~/id_rsa
 fi
