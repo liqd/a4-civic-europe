400011c4d23f40a87d9fba6b8eb20a3502d20a9c
Phillippa <p.morland@liqd.net>
2020-01-16 11:25:51 +0100

    Bootstrap4: update bootstrap v and variables and adhocracy v hash

--- civic_europe/assets/js/app.js
+++ civic_europe/assets/js/app.js
@@ -2,14 +2,19 @@
 
 var widget = require('adhocracy4').widget
 var ReactComments = require('adhocracy4').comments
+var ReactCommentsAsync = require('adhocracy4').comments_async
 var ReactFollow = require('../../../apps/follows/static/follows/react_follows.jsx')
 var ReactSupport = require('../../../apps/ideas/static/civic_europe_ideas/react_supports.jsx')
 var $ = window.jQuery = window.$ = require('jquery')
+
+// load bootstrap components
+require('bootstrap')
 
 require('../../../civic_europe/assets/js/civic_europe')
 
 $(function () {
   widget.initialise('a4', 'comment', ReactComments.renderComment)
+  widget.initialise('a4', 'comment_async', ReactCommentsAsync.renderComment)
   widget.initialise('ae', 'follows', ReactFollow.renderFollow)
   widget.initialise('ae', 'supports', ReactSupport.renderSupports)
 })
