91bff99defcb6cc33a228384d6d6b3a73a21d102
fuzzylogic2000 <katharina.albers@liqd.de>
2020-03-10 15:22:43 +0100

    apps/ideas/countries: change name of CZ

--- apps/ideas/countries.py
+++ apps/ideas/countries.py
@@ -1,7 +1,9 @@
 from django_countries import Countries
+from django.utils.translation import gettext_lazy as _
 
 
 class EuropeanCountries(Countries):
-    only = ['BG', 'HR', 'CZ', 'GR', 'HU', 'IT',
-            'PL', 'PT', 'RO', 'SK', 'SI', 'ES'
+    only = ['BG', 'HR', 'GR', 'HU', 'IT', 'PL',
+            'PT', 'RO', 'SK', 'SI', 'ES',
+            ('CZ', _('Czech republic'))
             ]
