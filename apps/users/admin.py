603e37e14555fd6d144b6404d87d5453897b430a
fuzzylogic2000 <katharina.albers@liqd.de>
2020-04-29 10:52:01 +0200

    apps/users: show get_notifications in admin

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -11,7 +11,8 @@ class UserAdmin(auth.admin.UserAdmin):
             'is_staff',
             'is_superuser',
             'groups',
-            'get_newsletters')}),
+            'get_newsletters',
+            'get_notifications')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
         (_('Profile data'), {'fields': (
             '_avatar',
