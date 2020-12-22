38c938c1f022be1617f97a5281060ccd69b11784
Rineee <k.schmid@liqd.net>
2020-12-22 19:36:43 +0100

    apps/ideas: change idea section of application form

--- apps/ideas/forms.py
+++ apps/ideas/forms.py
@@ -366,8 +366,9 @@ class IdeaSectionForm(BaseForm):
             'subtitle',
             'pitch',
             'image',
-            'topics',
-            'topics_other'
+            'country_of_implementation',
+            'field_of_action',
+            'field_of_action_other'
         ]
 
 
--- apps/ideas/models/sections/idea_section.py
+++ apps/ideas/models/sections/idea_section.py
@@ -1,96 +1,119 @@
 from django.db import models
 from django.utils.translation import ugettext as _
+from django_countries.fields import CountryField
 from multiselectfield import MultiSelectField
 
 from adhocracy4.images import fields
+from apps.ideas.countries import EuropeanCountries
 
-TITLE_HELP = _('Give your idea a short and '
-               'meaningful title (max. 50 characters).')
-PITCH_HELP = _('Share a concise and '
-               'appealing text that makes the reader curious. '
-               'Summarise your main challenge, objective, '
-               'target group, approach or activities, and intended '
-               'impact in 3-5 sentences. (max. 500 characters)')
-IMAGE_HELP = _('Upload a photo or illustration that visually '
-               'supports or explains your idea. '
-               'Make sure that you have the property rights '
-               'to share this picture. You can '
-               'upload a .jpg, .png or .gif up to 3 MB in size. '
-               'The image should be in '
-               'landscape (not portrait) format and have a width '
-               'of at least 400 pixels.')
-
-DEMOCRACY_AND_PARTICIPATION = 'DP'
-CIVIC_EDUCATION = 'CE'
 COMMUNITY_DEVELOPMENT = 'CD'
-URBAN_AND_RURAL_PLANING = 'UP'
 SOCIAL_INCLUSION = 'SI'
-MIGRATION = 'MI'
 ENVIRONMENT_AND_SUSTAINABILITY = 'ES'
 ARTS_AND_CULTURAL_ACTIVITIES = 'AC'
+HUMAN_RIGHTS = 'HR'
+YOUTH_PARTICIPATION = 'YP'
+JOURNALISM = 'JL'
+SOCIAL_ENTREPRENEURSHIP = 'SE'
+OTHER = 'OT'
 
-TOPIC_CHOICES = (
-    (DEMOCRACY_AND_PARTICIPATION, _('Democracy and participation')),
-    (CIVIC_EDUCATION, _('Civic education')),
+FIELD_OF_ACTION_CHOICES = (
     (COMMUNITY_DEVELOPMENT, _('Community development')),
-    (URBAN_AND_RURAL_PLANING, _('Urban and rural development')),
     (SOCIAL_INCLUSION, _('Social inclusion')),
-    (MIGRATION, _('Migration')),
     (ENVIRONMENT_AND_SUSTAINABILITY, _('Environment and sustainability')),
-    (ARTS_AND_CULTURAL_ACTIVITIES, _('Arts and cultural activities'))
+    (ARTS_AND_CULTURAL_ACTIVITIES, _('Arts and cultural activities')),
+    (HUMAN_RIGHTS, _('Human rights')),
+    (YOUTH_PARTICIPATION, _('Youth participation and empowerment')),
+    (JOURNALISM, _('Journalism')),
+    (SOCIAL_ENTREPRENEURSHIP, _('(Social) Entrepreneurship')),
+    (OTHER, _('Other'))
 )
 
-TOPIC_HELP = _('Choose 1-2 topics that fit to your idea. Your answer '
-               'to this question does not influence the selection process. '
-               'It helps to get a better overview of the ideas.')
+TITLE_HELP = _('Give your idea a short and '
+               'meaningful title (max. 50 characters). For instance: '
+               '“Islands of Hope”')
+SUBTITLE_HELP = _('Here you can add a short explanation of your title '
+                  '(max. 200 characters). For instance: “Fostering a '
+                  'culture of democratic dialogue and co-designing a '
+                  'vision for the development of a small island.”')
+PITCH_HELP = _('Share a concise and appealing text that summarizes your '
+               'idea, makes the reader want to learn more and that is '
+               'memorable. Summarize the challenge you are tackling, '
+               'your objective, target group and approach in 3-5 '
+               'sentences. (max. 500 characters)')
+IMAGE_HELP = _('Upload a photo or illustration that visually supports '
+               'or explains your idea. The picture will not influence '
+               'the selection process but it will be used as a header '
+               'picture of your idea on the website. Make sure that you '
+               'have the property rights to share this picture. You can '
+               'upload a .jpg, .png or .gif up to 3 MB in size. '
+               'The image should be in landscape (not portrait) format '
+               'and have a width of at least 400 pixels.')
+COUNTRY_OF_IMPLEMENTATION_HELP = _('You can choose multiple options if your '
+                                   'project takes place in more than one '
+                                   'country.')
+FIELD_OF_ACTION_HELP = _('Project ideas should be based on democratic values '
+                         'like human dignity, tolerance, freedom and '
+                         'diversity of opinion and the rule of law. They '
+                         'should also strengthen civic engagement and '
+                         'participation, ideally by means of civic '
+                         'education. Please choose 1-2 of the fields below '
+                         'that suit your idea. This will not influence the '
+                         'selection process, but will help users to filter '
+                         'the ideas in the idea space.')
 
 
 class IdeaSection(models.Model):
     title = models.CharField(
         max_length=50,
+        verbose_name=_('Idea title'),
         help_text=TITLE_HELP
     )
     subtitle = models.CharField(
         max_length=200,
-        help_text=_('(max. 200 characters)'),
+        verbose_name=_('Idea subtitle'),
+        help_text=SUBTITLE_HELP,
         blank=True
     )
     pitch = models.TextField(
         max_length=500,
+        verbose_name=_('Idea pitch'),
         help_text=PITCH_HELP
     )
     image = fields.ConfiguredImageField(
         'image',
         upload_to='ideas/images',
-        blank=True,
         verbose_name=_('Visualise your idea'),
-        help_text=IMAGE_HELP
+        help_text=IMAGE_HELP,
     )
-    topics = MultiSelectField(
+    country_of_implementation = CountryField(
+        countries=EuropeanCountries,
+        help_text=COUNTRY_OF_IMPLEMENTATION_HELP,
+        multiple=True
+    )
+    field_of_action = MultiSelectField(
         max_length=255,
-        choices=TOPIC_CHOICES,
+        choices=FIELD_OF_ACTION_CHOICES,
         max_choices=2,
-        help_text=TOPIC_HELP,
-        verbose_name=_('Topic')
+        help_text=FIELD_OF_ACTION_HELP
     )
-    topics_other = models.CharField(
-        max_length=250,
+    field_of_action_other = models.CharField(
+        max_length=50,
         blank=True,
-        verbose_name=_('Other'),
-        help_text=_('Please specify. (max 250 characters)')
+        verbose_name=_('If you chose “Other” please specify briefly here'),
+        help_text=_('(max. 50 characters)')
     )
 
     class Meta:
         abstract = True
 
     @property
-    def idea_topics_names(self):
-        choices = dict(TOPIC_CHOICES)
-        return [choices[topic] for topic in self.topics]
+    def idea_field_of_action_names(self):
+        choices = dict(FIELD_OF_ACTION_CHOICES)
+        return [choices[action] for action in self.field_of_action]
 
     @property
-    def all_idea_topics_names(self):
-        idea_topics = self.idea_topics_names
-        if self.topics_other:
-            idea_topics.append(self.topics_other)
-        return idea_topics
+    def all_idea_field_of_action_names(self):
+        idea_field_of_actions = self.idea_field_of_action_names
+        if self.field_of_action_other:
+            idea_field_of_actions.append(self.field_of_action_other)
+        return idea_field_of_actions
