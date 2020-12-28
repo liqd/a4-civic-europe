b5c3a2ccb251f741a979d0cc0307dc9c9a5c65eb
philli <phillippamorland@gmail.com>
2020-12-28 18:20:10 +0100

    sections/network_community_section: update helptext

--- apps/ideas/models/sections/network_community_section.py
+++ apps/ideas/models/sections/network_community_section.py
@@ -1,17 +1,23 @@
 from django.db import models
 from django.utils.translation import ugettext as _
 
-NETWORK_HELP = _('Winning projects meet at least one'
-                 ' time per year in network meetings and '
-                 'receive mentoring and support from the '
-                 'Civic Europe team. Tell us about your expectations. '
-                 'Think about your skills, resources, networks and '
-                 'partners when describing what you could offer and '
-                 'what you would like to take away. (max. 800 characters)')
+NETWORK_HELP = _('Winning projects meet at least '
+                 'once per year in network meetings '
+                 'and participate in peer-to-peer '
+                 'learning activities as well as '
+                 'trainings. Think about your skills, '
+                 'resources, networks and partners when '
+                 'describing what you could offer and '
+                 'what you would like to take away from '
+                 'such network meetings. (max. 800 characters)')
 
-FEEDBACK_HELP = _('What kind of advice, comments or feedback '
-                  'would you like to receive about your idea '
-                  'from others on the platform? (max. 300 characters)')
+FEEDBACK_HELP = _('Shortened versions of all ideas will be '
+                  'published in our idea space, an open space '
+                  'where registered users can comment on the '
+                  'ideas. What kind of advice, comments or '
+                  'feedback would you like to receive about '
+                  'your idea from others on the platform? '
+                  '(max. 300 characters)')
 
 
 class NetworkSection(models.Model):
