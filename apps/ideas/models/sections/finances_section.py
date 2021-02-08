b139ace8b583466fbc4d2bf523490c56daea0ac4
philli <phillippamorland@gmail.com>
2021-02-08 13:52:28 +0100

    ideas/finances_section: update budget amount and helptext fixes #375

--- apps/ideas/models/sections/finances_section.py
+++ apps/ideas/models/sections/finances_section.py
@@ -3,13 +3,16 @@ from django.db import models
 from django.utils.translation import ugettext as _
 
 TOTAL_BUDGET_HELP = _('Please indicate your overall budget. '
-                      'The total budget may (but does not have to) '
-                      'include the applicant’s own contribution '
-                      'and/or external sources of funding. This '
-                      'section will be published in the idea space.')
+                      'The total budget may (but does not '
+                      'have to) include the applicant’s own '
+                      'contribution and/or external sources '
+                      'of funding. Please only enter whole '
+                      'numbers, no decimal signs like points, '
+                      'commas or spaces. This section will be '
+                      'published in the idea space.')
 
 BUDGET_REQUESTED_HELP = _('Funding requested from Civic Europe '
-                          'can range from 1 to 50,000 EUR. Depending '
+                          'can range from 1 to 35000 EUR. Depending '
                           'on your planning, the amount entered here '
                           'can be the same as the “total budget” '
                           'figure entered above. This section will be '
@@ -37,7 +40,7 @@ class FinancesSection(models.Model):
         verbose_name=_('Funding requested from Civic Europe'),
         help_text=BUDGET_REQUESTED_HELP,
         validators=[
-            MaxValueValidator(50000),
+            MaxValueValidator(35000),
             MinValueValidator(0)
         ]
     )
