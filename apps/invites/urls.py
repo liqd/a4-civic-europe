6ca1b940555548376bec95b68eb44cbc088ddfa1
rine <k.schmid@liqd.net>
2022-03-25 18:44:49 +0100

    django 3 update: fix deprecation of django.conf.urls.url

--- apps/invites/urls.py
+++ apps/invites/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from . import models, views
 
@@ -7,12 +7,12 @@ def invite_urls(invite_model):
     view_name = invite_model.__name__.lower()
 
     return [
-        url(
+        re_path(
             r'^{}/(?P<invite_token>[-\w_]+)/$'.format(view_name),
             views.InviteDetailView.as_view(),
             name='{}-detail'.format(view_name)
         ),
-        url(
+        re_path(
             r'^{}/(?P<invite_token>[-\w_]+)/accept/$'.format(view_name),
             views.InviteUpdateView.as_view(),
             name='{}-update'.format(view_name)
