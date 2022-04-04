ee470daa7c3e11d7468fff042ec76187c0146e9e
Kha <k.tran@liqd.net>
2022-04-04 10:24:55 +0100

    apps/comments: changing bootstrap5 attrs to bootstrap4

--- civic_europe/assets/js/app.js
+++ civic_europe/assets/js/app.js
@@ -2,7 +2,6 @@
 
 const widget = require('adhocracy4').widget
 const ReactComments = require('adhocracy4').comments
-const ReactCommentsAsync = require('adhocracy4').comments_async
 const ReactFollow = require('../../../apps/follows/static/follows/react_follows.jsx')
 const ReactSupport = require('../../../apps/ideas/static/civic_europe_ideas/react_supports.jsx')
 const $ = window.jQuery = window.$ = require('jquery')
@@ -14,9 +13,10 @@ require('../../../civic_europe/assets/js/civic_europe')
 
 $(function () {
   widget.initialise('a4', 'comment', ReactComments.renderComment)
-  widget.initialise('a4', 'comment_async', ReactCommentsAsync.renderComment)
   widget.initialise('ae', 'follows', ReactFollow.renderFollow)
   widget.initialise('ae', 'supports', ReactSupport.renderSupports)
+  // changing bs5 elements to bs4 in civic europe
+  require('../../../civic_europe/assets/js/bootstrap5-fix')
 })
 
 const getCurrentPath = function () {
