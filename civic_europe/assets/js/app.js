50f9d4f73039b7be9a9327b1161bb9c1815a8978
Kha <k.tran@liqd.net>
2022-04-04 15:34:31 +0100

    wagtail/videoembed: adding missing classname and make sure all page types are working

--- civic_europe/assets/js/app.js
+++ civic_europe/assets/js/app.js
@@ -2,7 +2,6 @@
 
 const widget = require('adhocracy4').widget
 const ReactComments = require('adhocracy4').comments
-const ReactCommentsAsync = require('adhocracy4').comments_async
 const ReactFollow = require('../../../apps/follows/static/follows/react_follows.jsx')
 const ReactSupport = require('../../../apps/ideas/static/civic_europe_ideas/react_supports.jsx')
 const $ = window.jQuery = window.$ = require('jquery')
@@ -11,10 +10,10 @@ const $ = window.jQuery = window.$ = require('jquery')
 require('bootstrap')
 
 require('../../../civic_europe/assets/js/civic_europe')
+require('../../../civic_europe/assets/js/wagtail-videoembed-fix')
 
 $(function () {
   widget.initialise('a4', 'comment', ReactComments.renderComment)
-  widget.initialise('a4', 'comment_async', ReactCommentsAsync.renderComment)
   widget.initialise('ae', 'follows', ReactFollow.renderFollow)
   widget.initialise('ae', 'supports', ReactSupport.renderSupports)
 })
