042f53832923317837e542f7636255c1001b2e72
Kha <k.tran@liqd.net>
2022-04-05 11:08:28 +0100

    using methods getAttribute, setAttribute and removeAttribute, rather than mutating the dataset object

--- civic_europe/assets/js/bootstrap5-fix.js
+++ civic_europe/assets/js/bootstrap5-fix.js
@@ -1,29 +1,37 @@
+// This file runs on every load and checks if there are any bootstrap 5
+// data attributes on any DOM elements. It checks for data-bs-toggle,
+// data-bs-dismiss and data-bs-target attributes and converts them to
+// bootstrap 4 equivalent attributes.
+
 const bs5ToggleElements =
-  document.querySelectorAll('*[data-bs-toggle]')
+  document.querySelectorAll('[data-bs-toggle]')
 
 for (const toggleEl of bs5ToggleElements) {
-  if (toggleEl.dataset.bsToggle) {
-    toggleEl.dataset.toggle = toggleEl.dataset.bsToggle
-    delete toggleEl.dataset.bsToggle
+  const value = toggleEl.getAttribute('data-bs-toggle')
+  if (value) {
+    toggleEl.setAttribute('data-toggle', value)
+    toggleEl.removeAttribute('data-bs-toggle')
   }
 }
 
 const bs5DismissElements =
-  document.querySelectorAll('*[data-bs-dismiss]')
+  document.querySelectorAll('[data-bs-dismiss]')
 
 for (const dismissEl of bs5DismissElements) {
-  if (dismissEl.dataset.bsDismiss) {
-    dismissEl.dataset.dismiss = dismissEl.dataset.bsDismiss
-    delete dismissEl.dataset.bsDismiss
+  const value = dismissEl.getAttribute('data-bs-dismiss')
+  if (value) {
+    dismissEl.setAttribute('data-dismiss', value)
+    dismissEl.removeAttribute('data-bs-dismiss')
   }
 }
 
 const bs5TargetElements =
-  document.querySelectorAll('*[data-bs-target]')
+  document.querySelectorAll('[data-bs-target]')
 
 for (const targetEl of bs5TargetElements) {
-  if (targetEl.dataset.bsTarget) {
-    targetEl.dataset.target = targetEl.dataset.bsTarget
-    delete targetEl.dataset.bsTarget
+  const value = targetEl.getAttribute('data-bs-target')
+  if (value) {
+    targetEl.setAttribute('data-target', value)
+    targetEl.removeAttribute('data-bs-target')
   }
 }
